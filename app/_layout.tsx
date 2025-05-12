4f955f7de010bc74cb47938cc7f672efc5d12b1b
moska <moska@LAPTOP-CUSHTSLC>
2025-05-12 19:31:26 +0200

    Create colors files; import fonts (Refs #26)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,45 @@
-import { Stack } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
+import { Slot } from "expo-router";
+import { View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
-const RootLayout = () => {
-  return <Stack />;
-};
+SplashScreen.preventAutoHideAsync();
 
-export default RootLayout;
+export default function RootLayout() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "Nunito-Regular": require("../assets/fonts/Nunito-Regular.ttf"),
+          "Nunito-Bold": require("../assets/fonts/Nunito-Bold.ttf"),
+          "Nunito-Italic": require("../assets/fonts/Nunito-Italic.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Slot />
+    </View>
+  );
+}
---
+++ constants/style.ts
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    background: "#FAF9F6", // jasne tło
+    card: "#FFFFFF", // formularze / karty
+    primary: "#4CAF50", // główny zielony (np. przyciski)
+    primaryDark: "#388E3C", // ciemniejszy zielony (hover, aktywne)
+    accent: "#81C784", // dodatkowy zielony (np. zaznaczenia)
+    textPrimary: "#222222", // główny kolor tekstu
+    textSecondary: "#5A5A5A", // tekst pomocniczy (np. linki, labelki)
+    error: "#D32F2F", // czerwony do błędów
+    border: "#E0E0E0", // linie/obramowania inputów
+    icon: "#424242", // ikony nieaktywne
+    tabActive: "#4CAF50", // aktywna zakładka
+    tabInactive: "#5A5A5A", // nieaktywna zakładka
+  },
+};
