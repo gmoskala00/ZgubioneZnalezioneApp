8f9d2923eef725ee4bf679dabe3acb8d354a3455
moska <moska@LAPTOP-CUSHTSLC>
2025-12-06 12:33:17 +0100

    Deleted unneccesary line

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/found-items", foundItemRoutes);
 app.use("/api/claims", claimRoutes);
-app.use("/utils/fold");
 
 app.get("/", (_req, res) => {
   res.send("API LIVE");
